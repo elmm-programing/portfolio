65f7cc4ccf09ac71d8c29d39a15e96e74f012520
Edwin Levinson Mejia Marcelino <elmm.programing@gmail.com>
2025-05-07 21:29:23 -0400

    Refactor to don't use multiple language and Presentation change

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,13 +1,21 @@
 <template>
- <div id="home">
-      <div class="container mx-auto p-4 mt-[8rem] mb-[6rem]">
-        <h1 class="text-4xl font-bold mb-[1rem]">Edwin Levinson Mejia Marcelino</h1>
-     <p>{{ $t('+2 years of experience. Full Stack Developer from the Dominican Republic. Specialized in the development of web applications and with knowledge of backend, system design, software architecture and Data structures') }}</p>
- <Button type="button" class="mt-5 text-white border-white"  raised outlined as="a" href="https://www.linkedin.com/in/edwin-levinson-mej%C3%ADa-marcelino-738746201/" target="_blank" rel="noopener"  label="Linkedin" icon="pi pi-linkedin"  />
- <Button type="button" class="mt-5 mx-3 text-white border-white"  raised outlined as="a" href="https://github.com/elmm-programing" target="_blank" rel="noopener"  label="Github" icon="pi pi-github"  />
-      </div>
+  <div id="home">
+    <div class="container mx-auto p-4 mt-[8rem] mb-[6rem]">
+      <h1 class="text-4xl font-bold mb-[1rem]">Edwin Levinson Mejia Marcelino</h1>
+      <p>{{ `Software Developer with experience in backend and frontend development. Skilled in JavaScript, Vue.js,
+        Nuxt, React, Golang,and Docker.Proficient in project management and database administration.Adept at crafting
+        efficient and
+        responsive web designs.Experienced in Linux environment and proficient in Git for version control.Adept in
+        utilizing HTML & CSS, Neovim, and Postman for seamless Frontend development.Aiming to leverage expertise in Full
+        Stack Engineering to drive innovation and deliver high - quality solutions` }}</p>
+      <Button type="button" class="mt-5 text-white border-white" raised outlined as="a"
+        href="https://www.linkedin.com/in/edwin-levinson-mej%C3%ADa-marcelino-738746201/" target="_blank" rel="noopener"
+        label="Linkedin" icon="pi pi-linkedin" />
+      <Button type="button" class="mt-5 mx-3 text-white border-white" raised outlined as="a"
+        href="https://github.com/elmm-programing" target="_blank" rel="noopener" label="Github" icon="pi pi-github" />
+    </div>
 
-    </div> 
+  </div>
 </template>
 <script setup lang="ts">
 </script>
