86bce18832aefa94daa26a02506f7dc13e37a4ba
Edwin Levinson Mejia Marcelino <elmm.programing@gmail.com>
2025-05-07 21:28:20 -0400

    Experience refactor

--- components/Experience.vue
+++ components/Experience.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="experience" class="my-[4rem]">
-    <div class="container mx-auto p-4">
+  <div id="experience" class="my-[2rem]">
+    <div class="container mx-auto px-4">
       <h1 class="text-4xl font-bold mb-[4rem]">{{ $t('Work Experience') }}</h1>
       <Timeline :value="events" :pt="{ eventOpposite: { class: 'hidden' } }">
-        <template #marker="slotProps">
+        <template #marker="">
           <span class="flex w-3 h-3 items-center bg-white justify-center text-white rounded-full z-10 shadow-sm">
           </span>
         </template>
@@ -31,6 +31,6 @@
 <script setup lang="ts">
 const events = ref([
   { role: 'Full Stack Developer', company: 'Ministry of Finance of the Dominican Republic', date: 'At the moment...', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg' },
-  { role: 'Intern',company:'Ministry of Finance of the Dominican Republic', date: 'August 2022', icon: 'pi pi-cog', color: '#673AB7' },
+  { role: 'Intern', company: 'Ministry of Finance of the Dominican Republic', date: 'August 2022', icon: 'pi pi-cog', color: '#673AB7' },
 ]);
 </script>
